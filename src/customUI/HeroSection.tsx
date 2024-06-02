778a1f6800b2b551fe559b109650b6fca47de505
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-06-03 01:44:52 +0600

    feat: Update HeroSection Image Source
    - Changed the image source in the HeroSection component from an Unsplash URL to a local file (`../../syed-unsplash.jpg`).

--- src/customUI/HeroSection.tsx
+++ src/customUI/HeroSection.tsx
@@ -33,7 +33,7 @@ export function HeroSection() {
         <div className="rounded-lg bg-gray-200 p-4">
           <img
             className="aspect-[3/2] w-full rounded-lg bg-gray-50 object-cover lg:aspect-auto lg:h-[400px]"
-            src="https://images.unsplash.com/photo-1504384308090-c894fdcc538d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTc1fHx0ZWFtfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+            src="../../syed-unsplash.jpg"
             alt=""
           />
         </div>
