78d397f240677502a3abdcfd5eb762d1cafaf2ff
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-06-04 23:22:29 +0600

    feat: Refactor App component to include ThemeProvider and add background styling with gradients.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-// import "./App.css";
-// import { ThemeProvider } from "@/components/theme-provider";
-
+import "./App.css";
+import { ThemeProvider } from "@/components/theme-provider";
 import { HeroSection } from "./customUI/HeroSection";
 import { FeatureSection } from "./customUI/FeatureSection";
 import { Navbar } from "./customUI/Navbar";
@@ -10,14 +9,17 @@ import { FooterSection } from "./customUI/FooterSection";
 function App() {
   return (
     <>
-      {/* <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme"> */}
-      <Navbar />
-      <HeroSection />
-      <FeatureSection />
-      <ProductSection />
-      <FooterSection />
-
-      {/* </ThemeProvider> */}
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        {/* <div className="bg-gray-100 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]  dark:bg-slate-900 text-gray-800 dark:text-gray-100"> */}
+        <div className="relative h-full w-full bg-slate-950">
+          <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_1000px_at_70%_200px,#fed7aa,transparent)]"></div>
+          <Navbar />
+          <HeroSection />
+          <FeatureSection />
+          <ProductSection />
+          <FooterSection />
+        </div>
+      </ThemeProvider>
     </>
   );
 }
