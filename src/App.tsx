352c1c37a8afe067e2b79646a117827d6a13bd57
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-06-09 00:32:35 +0600

    feat: Update background gradient in App component
    - Modified the radial gradient in the main container to change its size and position.
    - Updated the gradient circle size from 1000px to 1200px and adjusted its position to 50% 100px.
    - Ensured the new background style blends seamlessly with the existing theme and layout.

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         {/* <div className="bg-gray-100 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]  dark:bg-slate-900 text-gray-800 dark:text-gray-100"> */}
         <div className="relative h-full w-full bg-slate-950">
-          <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_1000px_at_70%_200px,#fed7aa,transparent)]"></div>
+          <div className="absolute bottom-0  left-0 right-0 top-0 bg-[radial-gradient(circle_1200px_at_50%_100px,#fed7aa,transparent)]"></div>
           <Navbar />
           <HeroSection />
           <FeatureSection />
