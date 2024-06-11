18a3c660f804ac5629721069f9a435f7f75894e3
Muhāmmad Ranju <muhamadranju@gmail.com>
2024-06-12 00:33:50 +0600

    feat: Refactor ProductSection to dynamically load product data
    - Imported product data from `products.json`.
    - Replaced static product rendering with dynamic rendering using product data.
    - Updated image source and product details to use values from `products.json`.
    - Removed hardcoded hashtags and replaced with dynamic hashtags from product data.

--- src/customUI/ProductSection.tsx
+++ src/customUI/ProductSection.tsx
@@ -1,22 +1,27 @@
+import products from "../data/products.json";
 export function ProductSection() {
   return (
     <div className="mx-auto grid w-full max-w-7xl items-center space-y-4 px-2 py-10 md:grid-cols-2 md:gap-6 md:space-y-0 lg:grid-cols-4">
-      {Array.from({ length: 4 }).map((_, i) => (
-        <div key={i} className="rounded-md border">
+      {products.map((product) => (
+        <div key={product.description} className="rounded-md border">
           <img
-            src="../../annie-spratt-QOBHnWEg-mk-unsplash.jpg"
+            src={product.image}
             alt="Laptop"
-            className="aspect-[16/9] w-full rounded-md md:aspect-auto md:h-[300px] lg:h-[200px]"
+            className="aspect-[16/9] w-full rounded-md md:aspect-auto"
           />
           <div className="p-4">
             <h1 className="inline-flex items-center text-lg font-semibold">
-              Dish Pleat's
+              {product.title}
             </h1>
-            <p className="mt-3 text-sm ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Excepturi, debitis?
-            </p>
-            <div className="mt-4">
+            <p className="mt-3 text-sm ">{product.description}</p>
+            {product.hashTags.map((hashTag) => (
+              <div className="mt-4" key={hashTag}>
+                <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
+                  #{hashTag}
+                </span>
+              </div>
+            ))}
+            {/* <div className="mt-4">
               <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
                 #Sneakers
               </span>
@@ -26,7 +31,7 @@ export function ProductSection() {
               <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
                 #Airmax
               </span>
-            </div>
+            </div> */}
           </div>
         </div>
       ))}
